7565bbdd5569d2ba00413511e1b813aa150573ef
Santi Moreno <santismp8@gmail.com>
2023-09-20 23:56:33 +0200

    First commit: creating connector class with method

--- src/module_1/module_1_meteo_api.py
+++ src/module_1/module_1_meteo_api.py
@@ -1,4 +1,29 @@
 """ This is a dummy example """
+import pandas as pd
+import requests
+import matplotlib.pyplot as plt
+
+class APIConnector:
+
+    url = "https://open-meteo.com/en/docs/climate-api"
+
+    def __init__(self, url: str):
+        self._url = url
+    
+    def get_city(city: str):
+        match city:
+            case "Madrid":
+                return (40.4165, -3.7026)
+            case "Londres":
+                return (51.507351, -0.127758)
+            case "Rio de Janeiro":
+                return (-22.906847, -43.172896)
+            case _:
+                raise Exception("Invalid city")
+    
+
+        
+
 
 def main():
     raise NotImplementedError
