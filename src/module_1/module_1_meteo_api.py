33bb46d97052af1b6ce5ff3421b6870b2f65f308
dan1dr <dannieyo@gmail.com>
2023-09-21 23:00:44 +0200

    new version of process data + alternative approach

--- src/module_1/module_1_meteo_api.py
+++ src/module_1/module_1_meteo_api.py
@@ -6,6 +6,7 @@ import sys
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle 
 
 """Define global variables"""
 API_URL = "https://climate-api.open-meteo.com/v1/climate?"
@@ -27,7 +28,7 @@ MODELS = "CMCC_CM2_VHR4,FGOALS_f3_H,HiRAM_SIT_HR,MRI_AGCM3_2_S,EC_Earth3P_HR,MPI
 """Define auxiliar functions"""
 def call_api(url):
 #Need to add the verify=False as working from my corporate laptop. Tried to authenticate SSL by changing
-#lots of different things but issue still arises. Disabled SSL but only works from the office
+#lots of different things but issue still arises. Disabled SSL but only works from the office - not the best practice I know
     try:
         #to-do: add the cool off
         response = requests.get(url, verify=False)
@@ -69,40 +70,85 @@ def get_data_meteo_api(city, from_year, until_year):
     print("You reached the number of maximum attempts. Stopping the execution")
     sys.exit()
 
+def clean_data(raw_data):
+    cleaned_data = {}
+    for key, value in raw_data.items():
+        if value is not None:
+            if isinstance(value, dict):
+                cleaned_value = clean_data(value)  # Recursively clean sub-dictionaries starting again
+                if cleaned_value:  # Check if the sub-dictionary is not empty after cleaning
+                    cleaned_data[key] = cleaned_value
 
+            elif isinstance(value, list): #do the same but for list within the dict
+                # Clean each item in a list
+                cleaned_list = [item for item in value if item is not None]
+                if cleaned_list:
+                    cleaned_data[key] = cleaned_list
+            else:
+                cleaned_data[key] = value
+    return cleaned_data
 
 def process_data(data):
 
     #FIX!
     # Create an empty dict to store processed data
     processed_data = {}
-    # Extract the 'daily' data
-    count = 0
-    daily_data = data.get('daily', {})
-    for i, j in daily_data.items():
-        print(i, j)
-        count += 1
-        if count >=10:
-            break
+    daily_data = data['daily']
 
-    # Loop through each variable defined in VARIABLES
-    for variable in VARIABLES.split(','):
-        if variable in daily_data:
-            # Extract the 'time' and 'value' data for the variable
+    for variable in daily_data.keys():
+        for i in range(len(daily_data.keys())):
+
+        # Extract the 'time' and 'value' data for the variable
             variable_data = daily_data[variable]
+            time = variable_data[0]
+            values = variable_data[i]
 
-            time = variable_data.get('time', [])
-            values = variable_data.get(variable, [])
+        # Calculate average and deviation for this variable
+        #average = np.mean(values)
+        #deviation = np.std(values)
 
-            # Calculate average and deviation for this variable
-            average = np.mean(values)
-            deviation = np.std(values)
-
-            # Store the results in the processed_data dictionary
-            processed_data[variable] = {'average': average, 'deviation': deviation}
+        # Store the results in the processed_data dictionary
+        #processed_data[variable] = {'average': average, 'deviation': deviation}
 
     return processed_data
 
+def process_data_alternative(clean_data):
+    #trying another  solution ???
+    # Create an empty dict to store processed data
+    processed_data = {}
+    daily_data = clean_data['daily']
+    print(clean_data)
+    # Extract the 'daily' data
+    #print(daily_data)
+    #return daily_data
+    # Loop through each variable defined in VARIABLES
+    for index in range(len(daily_data['time'])):
+        lista = []
+        for variable in daily_data.keys():
+            #print(variable)
+            lista.append(daily_data[variable][index-1])
+
+            #variable_data = daily_data[variable]
+            
+            
+            processed_data[lista[0]] = lista[1:]
+            # Extract the 'time' and 'value' data for the variable
+            #print(processed_data)
+
+            #value_data_point = data_point[variable]
+            #time = variable_data[variable]
+            #values = variable_data[variable_data[0]]
+            #time.append(time_data_point)
+            #values.append(value_data_point)
+
+            # Calculate average and deviation for this variable
+            #average = np.mean(values)
+            #deviation = np.std(values)
+
+            # Store the results in the processed_data dictionary
+            #processed_data[variable] = {'average': average, 'deviation': deviation}
+    print(processed_data)
+        #return processed_data'''
 
 
 
@@ -111,15 +157,12 @@ def plot_data():
 
 def main():
     data = get_data_meteo_api("Madrid", 1950, 2050)
-    count = 0
-    #print(data.headers)
-    #print(data.text)
-    #print(data.json())
-    #print(json.dumps(data.json(), indent=5))
-    a = process_data(data)
-    #print(a)
+    cleaned_data = clean_data(data)
+    processed_data = process_data(cleaned_data)
+  
     
 
 if __name__ == "__main__":
     main()
     
+
