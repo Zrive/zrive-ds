d0e6a4d777dd32d2e17f8e3273acf77d6744497b
Ramon Coronado <ramoncormo8@gmail.com>
2023-09-22 19:24:16 +0200

    Meteo data appended(?)

--- src/module_1/module_1_meteo_api.py
+++ src/module_1/module_1_meteo_api.py
@@ -4,52 +4,44 @@
 import requests
 import pandas as pd
 import matplotlib as plt
-#import backoff
-#import time
+import backoff
+import time
+import json
+from jsonschema import validate, SchemaError
 
 
-#@backoff.on_exception(backoff.expo, request.error.RateLimitError)
+# Carga el archivo JSON en la variable schema_validation
+with open("/home/ramon/Zrive/zrive-ds/src/module_1/schema.json", 'r') as file:
+    schema_to_validate = json.load(file)
+
+@backoff.on_exception(backoff.expo, requests.exceptions.Timeout)
 def call_api(url, params=None):
     try:
         response = requests.get(url, params=params)
         response.raise_for_status() 
         return response.json()
     except requests.exceptions.RequestException as e:
-        print(f"Error al hacer la solicitud a la API: {e}")
+        print(f'Error al hacer la solicitud a la API: {e}')
         return None
 
-def get_data_meteo_api(city, API_URL):
-    params = {"city": city}
-    data = call_api(API_URL, params)
-    if data:
-        return data
-    else:
-        return None
+def get_data_meteo_api(city, coordinates, start_date, end_date, vars):
+    URL = f"https://climate-api.open-meteo.com/v1/climate?latitude={coordinates['latitude']}&longitude={coordinates['longitude']}&start_date={start_date}&end_date={end_date}&models=CMCC_CM2_VHR4,FGOALS_f3_H,HiRAM_SIT_HR,MRI_AGCM3_2_S,EC_Earth3P_HR,MPI_ESM1_2_XR,NICAM16_8S&daily={vars}"
+    data = call_api(URL)
+    try:
+        validate(data, schema_to_validate)
+    except SchemaError as e:
+        print("Error en el API_JSON schema")
+    finally:
+        df=pd.DataFrame(data=data['hourly'])
+        n_data = len(df)
+        city_name = city * n_data
+        df['city'] = city_name
+        time.sleep(10)
+    return df
 
-
-## def data_parse():
-#     #GETTING WEATHER FORECAST USING OPEN METEO API FOR EACH CITY
-#     data_list=[]
-#     for i in city.keys():
-#         url_data='https://api.open-meteo.com/v1/forecast?latitude='+str(city[i][0])+'&longitude='+str(city[i][1])+'&hourly='+variable+'&timezone=GMT&start_date='+start_date+'&end_date='+end_date
-#         response=requests.get(url_data).json() #get data response in json
-#         df=pd.DataFrame(response['hourly']) #convert data to pandas dataframe
-#         n_data=len(df)
-#         lat,lon=[city[i][0]]*n_data,[city[i][1]]*n_data #generate lon,lat for dataframe
-#         city_name=[i]*n_data
-#         df['lat']=lat
-#         df['lon']=lon
-#         df['city']=city_name #add city name to dataframe
-#         data_list.append(df)
-#         time.sleep(10) #pause 5 seconds before getting a new data
-
-    #JOIN ALL DATAFRAMES
-    data_con=pd.concat(data_list,axis=0)
-    data_con.head()
 
 def main():
-
-    URL = 'https://climate-api.open-meteo.com/v1/climate?latitude=52.52&longitude=13.41&start_date=1950-01-01&end_date=2050-12-31&models=CMCC_CM2_VHR4,FGOALS_f3_H,HiRAM_SIT_HR,MRI_AGCM3_2_S,EC_Earth3P_HR,MPI_ESM1_2_XR,NICAM16_8S&daily=temperature_2m_mean,precipitation_sum,soil_moisture_0_to_10cm_mean'
+        # Variables iniciales
     COORDINATES = {
     "Madrid": {"latitude": 40.416775, "longitude": -3.703790},
     "London": {"latitude": 51.507351, "longitude": -0.127758},
@@ -58,8 +50,15 @@ def main():
     start_date='1950-01-01'
     end_date='2023-08-30'
     VARIABLES = "temperature_2m_mean,precipitation_sum,soil_moisture_0_to_10cm_mean"
+        #####
+    data_cities = []
+    for i in COORDINATES.keys():
+        data_cities.append(get_data_meteo_api(city = i, coordinates = COORDINATES[i], start_date = start_date,
+                                              end_date = end_date, vars = VARIABLES))
+    meteo_data = pd.concat(data_cities, axis = 0)
+    meteo_data.head()
 
+    
 
 if __name__ == "__main__":
-    result = call_api(url = "https://open-meteo.com/en/docs/climate-api")
     main()
--- tests/module_1/test_meteo_api.py
+++ tests/module_1/test_meteo_api.py
@@ -4,11 +4,18 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__),'../../'))
 from src.module_1.module_1_meteo_api import call_api
+from src.module_1.module_1_meteo_api import get_data_meteo_api
 
 
-api_result = call_api(url = "https://climate-api.open-meteo.com/v1/climate?latitude=52.52&longitude=13.41&start_date=1950-01-01&end_date=2050-12-31&models=CMCC_CM2_VHR4,FGOALS_f3_H,HiRAM_SIT_HR,MRI_AGCM3_2_S,EC_Earth3P_HR,MPI_ESM1_2_XR,NICAM16_8S&daily=temperature_2m_mean,precipitation_sum,soil_moisture_0_to_10cm_mean")
-print(api_result)
+#api_result = call_api(url = "https://climate-api.open-meteo.com/v1/climate?latitude=52.52&longitude=13.41&start_date=1950-01-01&end_date=2050-12-31&models=CMCC_CM2_VHR4,FGOALS_f3_H,HiRAM_SIT_HR,MRI_AGCM3_2_S,EC_Earth3P_HR,MPI_ESM1_2_XR,NICAM16_8S&daily=temperature_2m_mean,precipitation_sum,soil_moisture_0_to_10cm_mean")
+#print(api_result)
 
-
-## def test_main():
+### def test_main():
 #     raise NotImplementedError
+
+COORDINAT = {
+    "Madrid": {"latitude": 40.416775, "longitude": -3.703790},
+    "London": {"latitude": 51.507351, "longitude": -0.127758},
+    "Rio": {"latitude": -22.906847, "longitude": -43.172896},
+    }
+print(COORDINAT["Madrid"]["latitude"])
