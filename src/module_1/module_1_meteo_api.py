46dba19a548c72b032526692a72f8d9fe0de427c
uhernando002 <uhernando002@ikasle.ehu.eus>
2023-09-22 14:27:18 +0200

    reduce temporal resolution (daily to yearly)

--- src/module_1/module_1_meteo_api.py
+++ src/module_1/module_1_meteo_api.py
@@ -13,22 +13,23 @@ END_DATE = "2050-12-31"
 
 
 def main():
-    # a = get_data_meteo_api("Madrid")
+    a = get_data_meteo_api("Madrid")
     # b = a["daily"]["time"]
     # print(b[:300])
     # print(a["daily"].keys())
-    a = [1, 3, 5, 7, 8]
-    b = get_data_meteo_api("Madrid")
-    c = b["daily"]["temperature_2m_mean"]
-    print(mean_calculation(c))
-    print(variance_calculation(c, mean_calculation(c)))
+    # a = [1, 3, 5, 7, 8]
+    # b = get_data_meteo_api("Madrid")
+    # c = b["daily"]["soil_moisture_0_to_10cm_mean"]
+    # print(mean_calculation(c))
+    # print(variance_calculation(c, mean_calculation(c)))
+    print(daily_to_yearly(a["daily"]))
 
 
 def get_data_meteo_api(city):
     param_city = COORDINATES[city]
     param_city["start_date"] = START_DATE
     param_city["end_date"] = END_DATE
-    param_city["daily"] = VARIABLES[0]
+    param_city["daily"] = VARIABLES
     data = api_connection(param_city)
     data_json = json.loads(data.text)
     return data_json
@@ -64,6 +65,42 @@ def variance_calculation(list, mean):
         suma = suma + c**2
     var = suma / len(list)
     return round(var, 3)
+
+
+def daily_to_yearly(daily):
+    actual_year = 1950
+    k = 0
+    yearly = {}
+    temp = list()
+    prec = list()
+    soil = list()
+    yearly = {
+        "time": [],
+        "temperature_2m_mean": [],
+        "precipitation_sum": [],
+        "soil_moisture_0_to_10cm_mean": [],
+    }
+
+    for day in daily["time"]:
+        year = day.split("-")
+        if actual_year != int(year[0]):
+            yearly["time"].append(actual_year)
+            yearly["temperature_2m_mean"].append(mean_calculation(temp))
+            yearly["precipitation_sum"].append(mean_calculation(prec))
+            yearly["soil_moisture_0_to_10cm_mean"].append(mean_calculation(soil))
+            actual_year = int(year[0])
+            temp.clear()
+            prec.clear()
+            soil.clear()
+        temp.append(daily["temperature_2m_mean"][k])
+        prec.append(daily["precipitation_sum"][k])
+        soil.append(daily["soil_moisture_0_to_10cm_mean"][k])
+        k = k + 1
+    return yearly
+
+
+def plotting(list):
+    a
 
 
 if __name__ == "__main__":
