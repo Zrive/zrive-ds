13aa2c5f51fecadc5c7c3d510f2b0f3b7306c9e5
Carlos <carlossantamariaizq@gmail.com>
2023-09-23 00:11:19 +0100

    Start plotting

--- src/module_1/module_1_meteo_api.py
+++ src/module_1/module_1_meteo_api.py
@@ -1,6 +1,7 @@
 import requests
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 # import time
 
@@ -16,6 +17,8 @@ MODELS = (
     "MRI_AGCM3_2_S,EC_Earth3P_HR,MPI_ESM1_2_XR,NICAM16_8S"
 )
 
+
+DATA_FOLDER_NAME = "Plots"
 
 def get_data_meteo_api(city, start_date, end_date):
     city_dict = COORDINATES[city]
@@ -40,42 +43,72 @@ def api_request(api_url):
         return response
 
 
-def plot_mean_and_std(data, time, data_key):
-    mean_value = np.mean(data)
-    # dispersion = abs(data - mean_value)  
+def plot_mean_and_std(mean_data, anual_mean_values, time, city, variable, model, plot_folder): 
 
-    plt.figure(figsize=(10, 6))
-    plt.scatter(time, data, label="Dispersion", color="b")
-    plt.axhline(y=mean_value, color="r", linestyle="--", label="Mean value")
+    y_values = anual_mean_values.values()
+    x_values = anual_mean_values.keys()
+
+    plt.figure(figsize=(12, 8))
+    plt.scatter(x_values, y_values, label="Dispersion", color="r")
+    plt.axhline(y=mean_data, color="b", linestyle="--", label="Mean value")
     plt.xlabel("Time")
     plt.ylabel("Data")
-    plt.title(data_key)
+    plt.xticks(rotation=45)
+    plt.title(f"Variable measured: {variable}. Model: {model}")
+    pic_filename = f"{plot_folder}/{city}_{variable}_{model}.png"
     plt.legend()
-    plt.show()
+    plt.savefig(pic_filename)
 
 
 def main():
-    city = "Madrid"
-    start_date = "2020-01-01"
-    end_date = "2021-01-01"
 
-    list_variables = VARIABLES.split(",")
-    list_models = MODELS.split(",")
+    start_date = "1950-01-01"
+    end_date = "2045-12-31"
 
-    variable = list_variables[1]
-    model = list_models[1]
-    data_key = f"{variable}_{model}"
 
-    api_url = get_data_meteo_api(city, start_date, end_date)
+    cities = COORDINATES.keys()
 
-    response = api_request(api_url)
+    for city in cities:
 
-    if response.status_code == 200:
-        data_dict = response.json()
-        data = data_dict["daily"][data_key]
-        time = data_dict["daily"]["time"]
+        plot_folder = f"{DATA_FOLDER_NAME}/{city}"
+        if os.path.exists(plot_folder) is False:
+            os.makedirs(plot_folder)
 
-        plot_mean_and_std(data, time, data_key)
+
+        list_variables = VARIABLES.split(",")
+        list_models = MODELS.split(",")
+
+        for variable, model in zip(list_variables, list_models):
+
+            data_key = f"{variable}_{model}"
+
+            api_url = get_data_meteo_api(city, start_date, end_date)
+
+            response = api_request(api_url)
+
+            if response.status_code == 200:
+                data_dict = response.json()
+                data = data_dict["daily"][data_key]
+                time = data_dict["daily"]["time"]
+                mean_data = np.mean(data)
+
+                sum_year_values = {}
+                count_year_values = {}
+
+                for value,date in zip(data, time):
+                    year = date.split("-")[0] # Output: [2020, 01, 01]
+
+                    if year in sum_year_values.keys():
+                        sum_year_values[year] += value
+                        count_year_values[year] +=1
+
+                    else:
+                        sum_year_values[year] = value
+                        count_year_values[year] = 1
+
+                anual_mean_values = {year: (sum/count) for year, sum, count in zip(sum_year_values.keys(), sum_year_values.values(), count_year_values.values())} # yearly dispersion from mean
+
+                plot_mean_and_std(mean_data, anual_mean_values, time, city, variable, model, plot_folder)
 
 
 if __name__ == "__main__":
