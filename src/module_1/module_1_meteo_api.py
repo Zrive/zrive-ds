1f089048e72b12e86f0ab9ecf2dd471a0005dff9
Carlos <carlossantamariaizq@gmail.com>
2023-09-23 01:34:28 +0100

    Testing pending

--- src/module_1/module_1_meteo_api.py
+++ src/module_1/module_1_meteo_api.py
@@ -1,8 +1,10 @@
 import requests
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 import os
 
+# flake8: noqa E501
 # import time
 
 API_URL = "https://climate-api.open-meteo.com/v1/climate?"
@@ -13,12 +15,14 @@ COORDINATES = {
 }
 VARIABLES = "temperature_2m_mean,precipitation_sum,soil_moisture_0_to_10cm_mean"
 MODELS = (
-    "CMCC_CM2_VHR4,FGOALS_f3_H,HiRAM_SIT_HR,"
-    "MRI_AGCM3_2_S,EC_Earth3P_HR,MPI_ESM1_2_XR,NICAM16_8S"
+    "CMCC_CM2_VHR4,FGOALS_f3_H,HiRAM_SIT_HR," "MRI_AGCM3_2_S,EC_Earth3P_HR,MPI_ESM1_2_XR,NICAM16_8S"
 )
+
+SLEEP_REQUEST = 1
 
 
 DATA_FOLDER_NAME = "Plots"
+
 
 def get_data_meteo_api(city, start_date, end_date):
     city_dict = COORDINATES[city]
@@ -37,22 +41,38 @@ def get_data_meteo_api(city, start_date, end_date):
 
 
 def api_request(api_url):
-    response = requests.get(api_url)
 
-    if response.status_code == 200:
-        return response
+    n_tries = 100
+
+    for _ in range(n_tries)
+        response = requests.get(api_url)
+        time.sleep(SLEEP_REQUEST)
+
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        
+        elif response.status_code == 404:
+            print("The data you requested does not exist. Tryng again")
+
+        elif response.status_code == 401:
+            print("You are not authorized to request this data. Trying again")
+
+        else:
+            print("Error. Trying again")
 
 
-def plot_mean_and_std(mean_data, anual_mean_values, time, city, variable, model, plot_folder): 
 
+def plot_mean_and_std(mean_data, anual_mean_values, time, city, variable, model, plot_folder):
     y_values = anual_mean_values.values()
     x_values = anual_mean_values.keys()
 
+    init_year = int(list(x_values)[0].split("-")[0])  # Gets the first year of dataset
+    end_year = int(list(x_values)[-1].split("-")[0])  # Gets the last year of dataset
 
-    init_year = int(list(x_values)[0].split("-")[0])
-    end_year = int(list(x_values)[-1].split("-")[0])
-
-    custom_x_labels = [str(year) for year in range(init_year, end_year, 2)]
+    custom_x_labels = [
+        str(year) for year in range(init_year, end_year, 2)
+    ]  # x label with two years spans
 
     plt.figure(figsize=(12, 8))
     plt.scatter(x_values, y_values, label="Dispersion", color="r")
@@ -70,28 +90,23 @@ def plot_mean_and_std(mean_data, anual_mean_values, time, city, variable, model,
 
 
 def main():
-
     start_date = "1950-01-01"
-    end_date = "2045-12-31"
-
+    end_date = "2049-12-31"
 
     cities = COORDINATES.keys()
 
     for city in cities:
-
         plot_folder = f"{DATA_FOLDER_NAME}/{city}"
         if os.path.exists(plot_folder) is False:
             os.makedirs(plot_folder)
-
 
         list_variables = VARIABLES.split(",")
         list_models = MODELS.split(",")
 
         for variable in list_variables:
             for model in list_models:
-
-                #model = "CMCC_CM2_VHR4"
-                #variable = "soil_moisture_0_to_10cm_mean"
+                # model = "CMCC_CM2_VHR4"
+                # variable = "soil_moisture_0_to_10cm_mean"
 
                 print(model, variable)
 
@@ -99,11 +114,10 @@ def main():
 
                 api_url = get_data_meteo_api(city, start_date, end_date)
 
-                response = api_request(api_url)
+                data_dict = api_request(api_url)
+                
+                if data:
 
-                if response.status_code == 200:
-
-                    data_dict = response.json()
                     data = data_dict["daily"][data_key]
                     time = data_dict["daily"]["time"]
                     try:
@@ -112,27 +126,32 @@ def main():
                         print(f"{variable} for {model} does not exist. Skip")
                         break
 
-
                     sum_year_values = {}
                     count_year_values = {}
 
-                    for value,date in zip(data, time):
-                        year = date.split("-")[0] # Output: [2020, 01, 01]
+                    for value, date in zip(data, time):
+                        year = date.split("-")[0]  # Output: [2020, 01, 01]
 
                         if year in sum_year_values.keys():
                             sum_year_values[year] += value
-                            count_year_values[year] +=1
+                            count_year_values[year] += 1
 
                         else:
                             sum_year_values[year] = value
                             count_year_values[year] = 1
 
-                    anual_mean_values = {year: (sum/count) for year, sum, count in zip(sum_year_values.keys(), sum_year_values.values(), count_year_values.values())} # yearly dispersion from mean
+                    anual_mean_values = {
+                        year: (sum / count)
+                        for year, sum, count in zip(
+                            sum_year_values.keys(),
+                            sum_year_values.values(),
+                            count_year_values.values(),
+                        )
+                    }  # Gets the anual dispersion from the mean
 
-                    plot_mean_and_std(mean_data, anual_mean_values, time, city, variable, model, plot_folder)
-
-                else:
-                    print(response.status_code)
+                    plot_mean_and_std(
+                        mean_data, anual_mean_values, time, city, variable, model, plot_folder
+                    )
 
 
 if __name__ == "__main__":
