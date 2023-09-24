ad145b87d06313f17679de02bb9f4d4123635774
Santi Moreno <santismp8@gmail.com>
2023-09-24 17:47:50 +0200

    Move functions out of the class

--- src/module_1/module_1_meteo_api.py
+++ src/module_1/module_1_meteo_api.py
@@ -73,96 +73,96 @@ class APIConnector:
 
         return df_response
     
-    # Calculate mean and dispersion (std & variance)
-    def calc_stats(df: pd.DataFrame, freq:str ="monthly") -> pd.DataFrame:
-        # Get data from df
-        time = df['daily'][0]
-        temp = df['daily'][1]
-        prec = df['daily'][2]
-        moist = df['daily'][3]
+# Calculate mean and dispersion (std & variance)
+def calc_stats(df: pd.DataFrame, freq:str ="monthly") -> pd.DataFrame:
+    # Get data from df
+    time = df['daily'][0]
+    temp = df['daily'][1]
+    prec = df['daily'][2]
+    moist = df['daily'][3]
+    
+    # New df with data in cols
+    working_df = pd.DataFrame()
+    working_df['date'] = time
+    working_df['temperature_2m_mean'] = temp
+    working_df['precipitation_sum'] = prec
+    working_df['soil_moisture_0_to_10cm_mean'] = moist
+    
+    # Check frequency of data
+    if freq == "monthly":
+        # Maybe parse the string using .split('-') could be another option, not sure what's best
+        working_df['date'] = pd.to_datetime(working_df['date'])
+        working_df['year'] = working_df['date'].dt.year
+        working_df['date'] = pd.to_datetime(working_df['date'])
+        working_df['month'] = working_df['date'].dt.month
+        working_df = working_df.drop(columns='date')
+
+        # Group by month and get stats
+        grouped_df = working_df.groupby(['year', 'month']).agg([np.mean, np.var, np.std]).reset_index()
+
+    # elif freq == "yearly":
+        # In case there's a chance to introduce more cases (semesters, trimesters, etc)
+    else:
+        working_df['date'] = pd.to_datetime(working_df['date'])
+        working_df['year'] = working_df['date'].dt.year
+        working_df = working_df.drop(columns='date')
+
+        # Group by year and get stats
+        grouped_df = working_df.groupby('year').agg([np.mean, np.var, np.std]).reset_index()
+
+    return grouped_df
+
+
+def paint_plots(dfs: list, plot: str, freq="monthly"):
+    
+    # It's all the same to use any to get the dates
+    if freq == "monthly":
+        x = dfs[0]['month']
+    else:
+        x = dfs[0]['year']
+    
+    # Since the grouped df is multi indexed and we want to compare
+    # every metric on their own (mean against mean, etc):
+    fig1, ax1 = plt.subplots(figsize=(9, 6))
+    fig2, ax2 = plt.subplots(figsize=(9, 6))
+    fig3, ax3 = plt.subplots(figsize=(9, 6))
+
+    ax1.set_title(f"Temperature: {plot}")
+    ax2.set_title(f"Rain: {plot}")
+    ax3.set_title(f"Moisture: {plot}")
+    
+    if plot == "mean":
+        for df in dfs:
+            y1 = df.loc[:, df.columns[1]] 
+            y2 = df.loc[:, df.columns[4]]
+            y3 = df.loc[: , df.columns[7]]
+                    
+            ax1.plot(x, y1)
+            ax2.plot(x, y2)
+            ax3.plot(x, y3)
+
+    elif plot == "var":
+        for df in dfs:
+            y1 = df.loc[:, df.columns[2]] 
+            y2 = df.loc[:, df.columns[5]]
+            y3 = df.loc[: , df.columns[8]]
         
-        # New df with data in cols
-        working_df = pd.DataFrame()
-        working_df['date'] = time
-        working_df['temperature_2m_mean'] = temp
-        working_df['precipitation_sum'] = prec
-        working_df['soil_moisture_0_to_10cm_mean'] = moist
+            ax1.plot(x, y1)
+            ax2.plot(x, y2)
+            ax3.plot(x, y3)
+    else:
+        for df in dfs:
+            y1 = df.loc[:, df.columns[3]] 
+            y2 = df.loc[:, df.columns[6]]
+            y3 = df.loc[: , df.columns[9]]
         
-        # Check frequency of data
-        if freq == "monthly":
-            # Maybe parse the string using .split('-') could be another option, not sure what's best
-            working_df['date'] = pd.to_datetime(working_df['date'])
-            working_df['year'] = working_df['date'].dt.year
-            working_df['date'] = pd.to_datetime(working_df['date'])
-            working_df['month'] = working_df['date'].dt.month
-            working_df = working_df.drop(columns='date')
+            ax1.plot(x, y1)
+            ax2.plot(x, y2)
+            ax3.plot(x, y3)
 
-            # Group by month and get stats
-            grouped_df = working_df.groupby(['year', 'month']).agg([np.mean, np.var, np.std]).reset_index()
-
-        # elif freq == "yearly":
-            # In case there's a chance to introduce more cases (semesters, trimesters, etc)
-        else:
-            working_df['date'] = pd.to_datetime(working_df['date'])
-            working_df['year'] = working_df['date'].dt.year
-            working_df = working_df.drop(columns='date')
-
-            # Group by year and get stats
-            grouped_df = working_df.groupby('year').agg([np.mean, np.var, np.std]).reset_index()
-
-        return grouped_df
-
-
-    def paint_plots(dfs: list, plot: str, freq="monthly"):
-        
-        # It's all the same to use any to get the dates
-        if freq == "monthly":
-            x = dfs[0]['month']
-        else:
-            x = dfs[0]['year']
-        
-        # Since the grouped df is multi indexed and we want to compare
-        # every metric on their own (mean against mean, etc):
-        fig1, ax1 = plt.subplots(figsize=(9, 6))
-        fig2, ax2 = plt.subplots(figsize=(9, 6))
-        fig3, ax3 = plt.subplots(figsize=(9, 6))
-
-        ax1.set_title(f"Temperature: {plot}")
-        ax2.set_title(f"Rain: {plot}")
-        ax3.set_title(f"Moisture: {plot}")
-        
-        if plot == "mean":
-            for df in dfs:
-                y1 = df.loc[:, df.columns[1]] 
-                y2 = df.loc[:, df.columns[4]]
-                y3 = df.loc[: , df.columns[7]]
-                        
-                ax1.plot(x, y1)
-                ax2.plot(x, y2)
-                ax3.plot(x, y3)
-
-        elif plot == "var":
-            for df in dfs:
-                y1 = df.loc[:, df.columns[2]] 
-                y2 = df.loc[:, df.columns[5]]
-                y3 = df.loc[: , df.columns[8]]
-            
-                ax1.plot(x, y1)
-                ax2.plot(x, y2)
-                ax3.plot(x, y3)
-        else:
-            for df in dfs:
-                y1 = df.loc[:, df.columns[3]] 
-                y2 = df.loc[:, df.columns[6]]
-                y3 = df.loc[: , df.columns[9]]
-            
-                ax1.plot(x, y1)
-                ax2.plot(x, y2)
-                ax3.plot(x, y3)
-
-        ax1.legend(["Madrid", "London", "Rio"])
-        ax2.legend(["Madrid", "London", "Rio"])
-        ax3.legend(["Madrid", "London", "Rio"])
+    ax1.legend(["Madrid", "London", "Rio"])
+    ax2.legend(["Madrid", "London", "Rio"])
+    ax3.legend(["Madrid", "London", "Rio"])
 
 def main():
     connector = APIConnector()
@@ -171,13 +171,13 @@ def main():
     df_londres = connector.get_data_meteo_api("London")
     df_rio = connector.get_data_meteo_api("Rio")
         
-    madrid = APIConnector.calc_stats(df_madrid)
-    londres = APIConnector.calc_stats(df_londres)
-    rio = APIConnector.calc_stats(df_rio)
+    madrid = calc_stats(df_madrid)
+    londres = calc_stats(df_londres)
+    rio = calc_stats(df_rio)
     
-    mean = APIConnector.paint_plots([madrid, londres, rio], "mean")
-    var = APIConnector.paint_plots([madrid, londres, rio], "var")
-    std = APIConnector.paint_plots([madrid, londres, rio], "std")
+    mean = paint_plots([madrid, londres, rio], "mean")
+    var = paint_plots([madrid, londres, rio], "var")
+    std = paint_plots([madrid, londres, rio], "std")
     
 
 
--- tests/module_1/test_meteo_api.py
+++ tests/module_1/test_meteo_api.py
@@ -1,7 +1,10 @@
 """ This is a dummy example to show how to import code from src/ for testing"""
 
 from src.module_1.module_1_meteo_api import main
+from src.module_1.module_1_meteo_api import APIConnector
+import pytest
+
 
 
 def test_main():
-    raise NotImplementedError
+    # We are going to test the calc_stats function
