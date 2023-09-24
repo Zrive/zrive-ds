c14b189cbf0379a254b69f80b2da92036dd1ff9d
Ramon Coronado <ramoncormo8@gmail.com>
2023-09-24 13:44:12 +0200

    Entrega final

--- src/module_1/module_1_meteo_api.py
+++ src/module_1/module_1_meteo_api.py
@@ -1,7 +1,6 @@
 ################################################
 ### Semana 1: Recoger datos API Meteorologia ###
 ################################################
-import numpy as np
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,25 +8,26 @@ import backoff
 import time
 import json
 import scipy.stats as stats
-import altair as alt
 from jsonschema import validate, SchemaError
 
 
 # Carga el archivo JSON en la variable schema_validation
-with open("/home/ramon/Zrive/zrive-ds/src/module_1/schema.json", 'r') as file:
+with open("/home/ramon/Zrive/zrive-ds/src/module_1/schema.json", "r") as file:
     schema_to_validate = json.load(file)
+
 
 @backoff.on_exception(backoff.expo, requests.exceptions.Timeout)
 def call_api(url, params=None):
     try:
         response = requests.get(url, params=params)
-        response.raise_for_status() 
+        response.raise_for_status()
         return response.json()
     except requests.exceptions.RequestException as e:
-        print(f'Error al hacer la solicitud a la API: {e}')
+        print(f"Error al hacer la solicitud a la API: {e}")
         return None
 
-def get_data_meteo_api(city, coordinates, start_date, end_date, vars):
+
+def get_data_meteo_api(coordinates, start_date, end_date, vars):
     URL = f"https://climate-api.open-meteo.com/v1/climate?latitude={coordinates['latitude']}&longitude={coordinates['longitude']}&start_date={start_date}&end_date={end_date}&models=CMCC_CM2_VHR4,FGOALS_f3_H,HiRAM_SIT_HR,MRI_AGCM3_2_S,EC_Earth3P_HR,MPI_ESM1_2_XR,NICAM16_8S&daily={vars}"
     data = call_api(URL)
     try:
@@ -35,72 +35,93 @@ def get_data_meteo_api(city, coordinates, start_date, end_date, vars):
     except SchemaError as e:
         print(f"Error en el API_JSON schema{e}")
     finally:
-        df=pd.DataFrame(data=data['daily'])
-        time.sleep(10)
+        df = pd.DataFrame(data=data["daily"])
     return df
 
+
 def data_parse(df, variables):
-    vars = [x.strip() for x in variables.split(',')]
-    resumen_df = df[['time']].copy()
+    vars = [x.strip() for x in variables.split(",")]
+    resumen_df = df[["time"]].copy()
     for variable in vars:
         modelos = [col for col in df.columns if variable in col]
         promedio_por_dia = df[modelos].mean(axis=1)
         resumen_df[variable] = promedio_por_dia
     return resumen_df
 
+
 def conf_interval(df):
-    df['time'] = pd.to_datetime(df['time'])
-    df = df.melt(id_vars = ['time'], var_name = 'Indicadores', value_name = 'Value')
-    df['Year'] = df['time'].dt.year
-    result = df.groupby(['Year', 'Indicadores'])['Value'].agg(['mean', 'std']).reset_index()
-    result['Lower_CI'] = result.apply(lambda row: row['mean'] - stats.t.ppf(1 - 0.05/ 2, df=len(df) - 1) * row['std'] / (len(df) ** 0.5), axis=1)
-    result['Upper_CI'] = result.apply(lambda row: row['mean'] + stats.t.ppf(1 - 0.05/ 2, df=len(df) - 1) * row['std'] / (len(df) ** 0.5), axis=1) 
+    df["time"] = pd.to_datetime(df["time"])
+    df = df.melt(id_vars=["time"], var_name="Indicadores", value_name="Value")
+    df["Year"] = df["time"].dt.year
+    result = (
+        df.groupby(["Year", "Indicadores"])["Value"].agg(["mean", "std"]).reset_index()
+    )
+    result["Lower_CI"] = result.apply(
+        lambda row: row["mean"]
+        - stats.t.ppf(1 - 0.05 / 2, df=len(df) - 1) * row["std"] / (len(df) ** 0.5),
+        axis=1,
+    )
+    result["Upper_CI"] = result.apply(
+        lambda row: row["mean"]
+        + stats.t.ppf(1 - 0.05 / 2, df=len(df) - 1) * row["std"] / (len(df) ** 0.5),
+        axis=1,
+    )
     return result
 
+
 def graficar(df, variables):
-    vars = [x.strip() for x in variables.split(',')]
+    vars = [x.strip() for x in variables.split(",")]
     for variable in vars:
         df_filtered = df[df["Indicadores"] == variable]
-        cities = df_filtered['city'].unique()
+        cities = df_filtered["city"].unique()
         fig, ax = plt.subplots(figsize=(10, 6))
         for city in cities:
-            city_data = df_filtered[df_filtered['city'] == city]
-            ax.plot(city_data['Year'], city_data['mean'], label=city)
-            ax.fill_between(city_data['Year'], city_data['Lower_CI'], city_data['Upper_CI'], alpha=0.3)
-        
-        ax.set_xlabel('Year')
-        ax.set_ylabel('Value')
-        ax.set_title(f'{variable} with Confidence Intervals by City')
-        ax.legend(loc='upper left')
+            city_data = df_filtered[df_filtered["city"] == city]
+            ax.plot(city_data["Year"], city_data["mean"], label=city)
+            ax.fill_between(
+                city_data["Year"],
+                city_data["Lower_CI"],
+                city_data["Upper_CI"],
+                alpha=0.3,
+            )
 
-        fig.savefig(f'{variable}_meteo.jpg', format='jpg')
+        ax.set_xlabel("Year")
+        ax.set_ylabel("Value")
+        ax.set_title(f"{variable} with Confidence Intervals by City")
+        ax.legend(loc="upper left")
+
+        fig.savefig(f"{variable}_meteo.jpg", format="jpg")
+
 
 def main():
-        # Variables iniciales
+    # Variables iniciales
     COORDINATES = {
-    "Madrid": {"latitude": 40.416775, "longitude": -3.703790},
-    "London": {"latitude": 51.507351, "longitude": -0.127758},
-    "Rio": {"latitude": -22.906847, "longitude": -43.172896},
+        "Madrid": {"latitude": 40.416775, "longitude": -3.703790},
+        "London": {"latitude": 51.507351, "longitude": -0.127758},
+        "Rio": {"latitude": -22.906847, "longitude": -43.172896},
     }
-    start_date='1950-01-01'
-    end_date='2023-08-30'
+    start_date = "1950-01-01"
+    end_date = "2050-01-01"
     VARIABLES = "temperature_2m_mean,precipitation_sum,soil_moisture_0_to_10cm_mean"
-        ###########################
-            ##################
-        ###########################
     data_cities = []
     for i in COORDINATES.keys():
-        city_data = get_data_meteo_api(city = i, coordinates = COORDINATES[i], start_date = start_date,
-                                              end_date = end_date, vars = VARIABLES)
-        viariable_data = data_parse(df = city_data, variables = VARIABLES)
-        variable_intervals = conf_interval(df = viariable_data)
-        variable_intervals['city'] = i
+        city_data = get_data_meteo_api(
+            coordinates=COORDINATES[i],
+            start_date=start_date,
+            end_date=end_date,
+            vars=VARIABLES,
+        )
+        viariable_data = data_parse(df=city_data, variables=VARIABLES)
+        variable_intervals = conf_interval(df=viariable_data)
+        variable_intervals["city"] = i
         data_cities.append(variable_intervals)
-    meteo_data = pd.concat(data_cities, axis = 0)
-    graficar(df = meteo_data, variables = VARIABLES)
+        time.sleep(10)
+    meteo_data = pd.concat(data_cities, axis=0)
+    graficar(df=meteo_data, variables=VARIABLES)
+
 
 if __name__ == "__main__":
     start_time = time.time()
     main()
     end_time = time.time()
-    print (f'Timepo de ejecutción: {end_time-start_time}s')
+    print(f"Timepo de ejecutción: {end_time-start_time}s")
--- tests/module_1/test_meteo_api.py
+++ tests/module_1/test_meteo_api.py
@@ -1,23 +1,41 @@
 """ This is a dummy example to show how to import code from src/ for testing"""
-import requests
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__),'../../'))
+import time
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))
 from src.module_1.module_1_meteo_api import call_api
 from src.module_1.module_1_meteo_api import get_data_meteo_api
+from src.module_1.module_1_meteo_api import data_parse
+from src.module_1.module_1_meteo_api import conf_interval
 
-#Variables 
-COORDINATES = {
-"Madrid": {"latitude": 40.416775, "longitude": -3.703790},
-"London": {"latitude": 51.507351, "longitude": -0.127758},
-"Rio": {"latitude": -22.906847, "longitude": -43.172896},
-}
-start_date='1950-01-01'
-end_date='2023-08-30'
-VARIABLES = "temperature_2m_mean,precipitation_sum,soil_moisture_0_to_10cm_mean"
 
-api_result = call_api(url= f"https://climate-api.open-meteo.com/v1/climate?latitude={COORDINATES['Madrid']['latitude']}&longitude={COORDINATES['Madrid']['longitude']}&start_date={start_date}&end_date={end_date}&models=CMCC_CM2_VHR4,FGOALS_f3_H,HiRAM_SIT_HR,MRI_AGCM3_2_S,EC_Earth3P_HR,MPI_ESM1_2_XR,NICAM16_8S&daily={VARIABLES}")
-print(api_result)
+def mean_test(df):
+    df2 = df.copy()
+    df2["Avg"] = df2.mean(numeric_only=True, axis=1)
+    return df2["Avg"].mean(), df2["Avg"].std()
 
-### def test_main():
-#     raise NotImplementedError
+
+def test_main():
+    COORDINATES = {"Madrid": {"latitude": 40.416775, "longitude": -3.703790}}
+    start_date = "2022-01-01"
+    end_date = "2022-12-31"
+    VARIABLES = "temperature_2m_mean"
+    city_data = get_data_meteo_api(
+        coordinates=COORDINATES["Madrid"],
+        start_date=start_date,
+        end_date=end_date,
+        vars=VARIABLES,
+    )
+    viariable_data = data_parse(df=city_data, variables=VARIABLES)
+    test_mean = mean_test(city_data)
+    variable_intervals = conf_interval(df=viariable_data)
+    print(variable_intervals.head())
+    print(test_mean)
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    test_main()
+    end_time = time.time()
+    print(f"Timepo de ejecutción: {end_time-start_time}s")
