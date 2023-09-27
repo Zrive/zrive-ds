decb9a7b6e55eeefb756f23254d01ee989dae86f
Santi Moreno <santismp8@gmail.com>
2023-09-28 00:44:07 +0200

    Execute black on meteo api

--- src/module_1/module_1_meteo_api.py
+++ src/module_1/module_1_meteo_api.py
@@ -13,7 +13,7 @@ COORDINATES = {
 
 MODELS = "CMCC_CM2_VHR4,FGOALS_f3_H,HiRAM_SIT_HR"
 
-VARIABLES = "temperature_2m_mean,precipitation_sum,soil_moisture_0_to_10cm_mean" 
+VARIABLES = "temperature_2m_mean,precipitation_sum,soil_moisture_0_to_10cm_mean"
 
 
 class APIConnector:
@@ -84,42 +84,42 @@ class APIConnector:
 
 
 def get_df_index() -> List:
-    models = MODELS.split(',')
-    variables = VARIABLES.split(',')
+    models = MODELS.split(",")
+    variables = VARIABLES.split(",")
     unjoined_index = list(itertools.product(variables, models))
     joined_index = []
     for item in unjoined_index:
-        joined_index.append('_'.join(item))
+        joined_index.append("_".join(item))
     return joined_index
 
 
 # Calculate mean and dispersion (std & variance)
-    # Calculate mean and dispersion
+# Calculate mean and dispersion
 def calc_stats(df: pd.DataFrame) -> pd.DataFrame:
     index = get_df_index()
 
-    models = MODELS.split(',')
+    models = MODELS.split(",")
     # Get data from df
     time = df["daily"]["time"]
-    df_models = pd.DataFrame() 
+    df_models = pd.DataFrame()
     df_models["Date"] = time
     df_models["Year"] = pd.to_datetime(df_models["Date"]).dt.year
     df_models.drop(columns="Date", inplace=True)
-    
+
     # Get data from index
     for item in index:
         if models[0] in item:
-            col_name = item.replace(models[0], 'model1')
+            col_name = item.replace(models[0], "model1")
             df_models[col_name] = df["daily"][item]
         elif models[1] in item:
-            col_name = item.replace(models[1], 'model2')
+            col_name = item.replace(models[1], "model2")
             df_models[col_name] = df["daily"][item]
         elif models[2] in item:
-            col_name = item.replace(models[2], 'model3')
+            col_name = item.replace(models[2], "model3")
             df_models[col_name] = df["daily"][item]
-    
+
     # Group by year and get stats
-    grouped_df = df_models.groupby("Year").agg(['mean', 'std']).reset_index()
+    grouped_df = df_models.groupby("Year").agg(["mean", "std"]).reset_index()
 
     return grouped_df
 
@@ -136,13 +136,12 @@ def paint_plots(df: pd.DataFrame, city: str):
     # Get the columns, since it is multiindexed, we check the first element
     # of the tuple (variable_name, stat)
     matching_temp = [col for col in df.columns if "temp" in col[0]]
-    matching_moist = [col for col in df.columns if "moist" in col[0]]
+    # matching_moist = [col for col in df.columns if "moist" in col[0]]
     matching_precip = [col for col in df.columns if "precip" in col[0]]
 
     df_temp = df[matching_temp]
-    df_moist = df[matching_moist]
+    # df_moist = df[matching_moist]
     df_precip = df[matching_precip]
-
 
     y1 = df_temp.iloc[:, df_temp.columns.get_level_values(1) == "mean"]
     # y2 = df_moist.iloc[:, df_moist.columns.get_level_values(1) == "mean"]
@@ -151,43 +150,42 @@ def paint_plots(df: pd.DataFrame, city: str):
     std1 = df_temp.iloc[:, df_temp.columns.get_level_values(1) == "std"]
     # std2 = df_moist.iloc[:, df_moist.columns.get_level_values(1) == "std"]
     std3 = df_precip.iloc[:, df_precip.columns.get_level_values(1) == "std"]
-    
+
     ax1.plot(x, y1)
     ax1.set_ylim(10, 30)
     ax1.plot(x, y1 + std1)
     ax1.plot(x, y1 - std1)
     ax1.set_title(f"{city} - Temperature")
-    ax1.legend(MODELS.split(','))
-    
+    ax1.legend(MODELS.split(","))
+    ax1.savefig(f"{city}_temp.png")
 
     # ax2.plot(x, y2)
     # ax2.plot(x, y2 + std2)
     # ax2.plot(x, y2 - std2)
-
 
     ax3.plot(x, y3)
     ax3.set_ylim(0, 10)
     ax3.plot(x, y3 + std3)
     ax3.plot(x, y3 - std3)
     ax3.set_title(f"{city} - Precipitation")
-    ax3.legend(MODELS.split(','))
+    ax3.legend(MODELS.split(","))
+    ax3.savefig(f"{city}_precip.png")
 
 
 def main():
     connector = APIConnector()
 
     df_madrid = connector.get_data_meteo_api("Madrid")
-    #df_londres = connector.get_data_meteo_api("London")
-    #df_rio = connector.get_data_meteo_api("Rio")
+    df_londres = connector.get_data_meteo_api("London")
+    df_rio = connector.get_data_meteo_api("Rio")
 
-    calc_stats(df_madrid)
-    # londres = calc_stats(df_londres)
-    #rio = calc_stats(df_rio)
+    madrid = calc_stats(df_madrid)
+    londres = calc_stats(df_londres)
+    rio = calc_stats(df_rio)
 
-
-    #paint_plots([madrid, londres, rio], "mean")
-    #paint_plots([madrid, londres, rio], "var")
-    #paint_plots([madrid, londres, rio], "std")
+    paint_plots(madrid, "Madrid")
+    paint_plots(londres, "Londres")
+    paint_plots(rio, "Rio")
 
 
 if __name__ == "__main__":
