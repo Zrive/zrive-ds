50f716426575e7aac270d8ef013ed398b3771948
alvarochapela <alvacha12@me.com>
2023-10-02 00:30:46 +0200

    All the data downloaded

---
+++ src/module_2/module_2_eda.py
@@ -0,0 +1,36 @@
+import boto3
+import os
+
+# AWS credentials
+aws_access_key_id = 'AKIAXN64CPXKVY56HGZZ'
+aws_secret_access_key = '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'
+
+# Initialize a session with boto3
+session = boto3.Session(
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+)
+
+# Create an S3 client
+s3 = session.client('s3')
+
+# Define the S3 bucket and objects you want to download
+s3_bucket = 'zrive-ds-data'
+s3_prefix = 'groceries/sampled-datasets/'
+
+# Define a local directory to save the downloaded files
+local_directory = 'Module2Data'
+
+# Create the local directory if it doesn't exist
+os.makedirs(local_directory, exist_ok=True)
+
+# List files in the S3 bucket
+response = s3.list_objects_v2(Bucket=s3_bucket, Prefix=s3_prefix)
+
+# Download each file from S3 to the local directory
+for obj in response.get('Contents', []):
+    s3_object_key = obj['Key']
+    local_file_path = os.path.join(local_directory, os.path.basename(s3_object_key))
+    s3.download_file(s3_bucket, s3_object_key, local_file_path)
+
+
