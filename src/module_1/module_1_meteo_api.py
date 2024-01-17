6d0c32bed2f5bc2707f98085fded2c2d7da41de2
em4go <emagodev@gmail.com>
2024-01-17 21:33:17 +0100

    add code template

--- src/module_1/module_1_meteo_api.py
+++ src/module_1/module_1_meteo_api.py
@@ -1,7 +1,17 @@
-""" This is a dummy example """
+API_URL = "https://climate-api.open-meteo.com/v1/climate?"
+
+COORDINATES = {
+    "Madrid": {"latitude": 40.416775, "longitude": -3.703790},
+    "London": {"latitude": 51.507351, "longitude": -0.127758},
+    "Rio": {"latitude": -22.906847, "longitude": -43.172896},
+}
+
+VARIABLES = "temperature_2m_mean,precipitation_sum,soil_moisture_0_to_10cm_mean"
+
 
 def main():
     raise NotImplementedError
+
 
 if __name__ == "__main__":
     main()
