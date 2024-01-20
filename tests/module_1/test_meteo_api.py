e019305641efdd3d696f3f201450b755abc3c890
em4go <emagodev@gmail.com>
2024-01-20 19:58:25 +0100

    Add test for join_models_mean_std function

--- tests/module_1/test_meteo_api.py
+++ tests/module_1/test_meteo_api.py
@@ -1,7 +1,25 @@
 """ This is a dummy example to show how to import code from src/ for testing"""
 
-from src.module_1.module_1_meteo_api import main
+from src.module_1.module_1_meteo_api import (
+    join_models_mean_std,
+)
+import pandas as pd
+import pytest
 
 
-def test_main():
-    raise NotImplementedError
+def test_join_models_mean_std():
+    input_data = {
+        "city": ["Madrid", "Madrid"],
+        "year": [2020, 2021],
+        "temperature_2m_mean_CMCC_CM2_VHR4": [20, 21],
+        "temperature_2m_mean_FGOALS_f3_H": [19, 20],
+    }
+    df = pd.DataFrame(input_data)
+    result_df = join_models_mean_std(df)
+
+    assert all(
+        column in result_df.columns
+        for column in ["temperature_2m_mean_mean", "temperature_2m_mean_std"]
+    )
+    assert result_df["temperature_2m_mean_mean"].iloc[0] == 19.5
+    assert result_df["temperature_2m_mean_std"].iloc[0] == pytest.approx(0.707, 0.001)
