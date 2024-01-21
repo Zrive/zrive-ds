c87c9facdaee2ca979b83900a4b4c445f035ba2c
“Alvaro_Leal” <alvarolealdz@gmail.com>
2024-01-21 22:58:41 +0100

    Ultimo commit antes de hacer la segunda PR

--- src/module_1/module_1_meteo_api.py
+++ src/module_1/module_1_meteo_api.py
@@ -16,15 +16,15 @@ COORDINATES = {
 # Funcion para descargar los datos de la API
 def get_data_meteo_api():
     results = {}
-    end_date = "2024-01-20"
-    start_date = (datetime.strptime(end_date, "%Y-%m-%d") - timedelta(days=30)).strftime("%Y-%m-%d") # Vamos a trabajar con los datos de los ultimos 30 dias
+    end_date = "2050-01-01"
+    start_date = "1950-01-01"
     for city, coords in COORDINATES.items():
         params = {
             'latitude': coords['latitude'],
             'longitude': coords['longitude'],
             'start_date': start_date,
             'end_date': end_date,
-            "models": ["FGOALS_f3_H"], # Mejorar incluyendo el resto de modelos
+            "models": ["MRI-AGCM3-2-S"], # Mejorar incluyendo el resto de modelos
             'daily': VARIABLES
         }
         try:
@@ -49,12 +49,12 @@ def get_data_meteo_api():
 # Dataset creado:
 all_city_data = get_data_meteo_api()
 for city, df in all_city_data.items():
-  #  print(city)
-  #  print(df)
-    
-# Función para representar el gráfico de dispersión
- def plot_city_data(city, df):
-    df_city = df[df['city'] == city]
+  print(city)
+  print(df)
+
+# Función para representar el gráfico de dispersión - No funciona
+ def plot_city_data(city_name, df):
+    df_city = df[df['city_name'] == city_name]
     df_city['time'] = pd.to_datetime(df_city['time'])
     variables = ['temperature_2m_mean', 'precipitation_sum', 'soil_moisture_0_to_10cm_mean']
     
@@ -63,8 +63,8 @@ for city, df in all_city_data.items():
         
         # Gráfico de dispersión
         plt.figure(figsize=(10, 6))
-        plt.scatter(df_var['time'], df_var[variable], label=f'{variable} en {city}')
-        plt.title(f'Evolución de {variable} en {city}')
+        plt.scatter(df_var['time'], df_var[variable], label=f'{variable} en {city_name}')
+        plt.title(f'Evolución de {variable} en {city_name}')
         plt.xlabel('Tiempo')
         plt.ylabel(variable)
         plt.legend()
@@ -73,5 +73,11 @@ for city, df in all_city_data.items():
         plt.show()
 
 plot_city_data('Madrid', df)
+
+#Funcion principal por definir
+def main():
+
+if __name__ == "__main__":
+main()
 
 # Primera versión del código a mejorar: Incompleto.
--- tests/module_1/test_meteo_api.py
+++ tests/module_1/test_meteo_api.py
@@ -1,7 +1,8 @@
 """ This is a dummy example to show how to import code from src/ for testing"""
 
-from src.module_1.module_1_meteo_api import main
+from src.module_1.module_1_meteo_api import get_data_meteo_api #Error: ModuleNotFoundError: No module named 'tests'
 
-
-def test_main():
-    raise NotImplementedError
+all_city_data = get_data_meteo_api()
+for city, df in all_city_data.items():
+  print(city)
+  print(df)
