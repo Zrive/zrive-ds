bfd5ebb08d42d6a3bfce87511858144150486671
Alae <kenaivideos@gmail.com>
2024-04-21 10:08:17 +0200

    modified:   evol.py 	modified:   tests/module_1/test_meteo_api.py

--- evol.py
+++ evol.py
@@ -3,12 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+
+
 API_URL = "https://climate-api.open-meteo.com/v1/climate?"
 
 COORDINATES = {
     "Madrid": {"latitude": 40.416775, "longitude": -3.703790},
+    "London": {"latitude": 51.507351, "longitude": -0.127758},
+    "Rio": {"latitude": -22.906847, "longitude": -43.172896},
 }
-VARIABLES = "temperature_2m_mean"
+VARIABLES = "temperature_2m_mean,precipitation_sum,soil_moisture_0_to_10cm_mean"
 
 
 #primera funcion, recogida de datos
@@ -20,7 +24,7 @@ def get_data_meteo_api(city, start_year, end_year):
         "longitude": COORDINATES[city]["longitude"],
         "start": f"{start_year}-01-01",
         "end": f"{end_year}-12-31",
-        "models": ["MRI_AGCM3_2_S"],
+        "models": ["MRI_AGCM3_2_S", "EC_Earth3P_HR"],
         "daily": VARIABLES
     }
     response = requests.get(API_URL, params=params)
@@ -30,7 +34,7 @@ def get_data_meteo_api(city, start_year, end_year):
 
 
 def main():
-    cities = ["Madrid"]
+    cities = ["Madrid", "London", "Rio"]
     start_year = 1950
     end_year = 2050
 
@@ -38,6 +42,10 @@ def main():
     for city in cities:
         data = get_data_meteo_api(city, start_year, end_year)
         print(data)
+
+    #hola
+    print("hola")
+    
 
 
 if __name__ == "__main__":
---
+++ tests/module_1/test_meteo_api.py
@@ -0,0 +1,1 @@
+#intento de commit
